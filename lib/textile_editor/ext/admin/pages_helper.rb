be8a19d6c5509f2d790e067e3fe8298536b25dc3
Jason Garber <jg@jasongarber.com>
2009-03-04 23:34:28 -0500

    Update javascript to be compatible with Radiant 0.7

--- lib/textile_editor/ext/admin/pages_helper.rb
+++ lib/textile_editor/ext/admin/pages_helper.rb
@@ -10,8 +10,8 @@ module TextileEditor
         page_edit_javascripts_without_textile_editor + <<-CODE
         part_added_without_textile_editor_observation = part_added;
         function part_added_with_textile_editor_observation() {
-          var index = parseInt($('part-index-field').value) + 1;
-          var page = 'page-' + index;
+          var partName = $F('part-name-field');
+          var page = 'page-' + partName.toSlug();
           part_added_without_textile_editor_observation();
           new filterObserver( $(page).getElementsByClassName('textarea')[0] );
         }
