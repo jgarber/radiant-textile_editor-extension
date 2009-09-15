7f91f987de138102ca410700302dbfc18b5317cc
Jason Garber <jg@jasongarber.com>
2009-09-15 14:07:57 -0400

    Version bump.

--- textile_editor_extension.rb
+++ textile_editor_extension.rb
@@ -2,7 +2,7 @@
 require_dependency 'application_controller'
 
 class TextileEditorExtension < Radiant::Extension
-  version "2.0"
+  version "2.1"
   description "Places a toolbar above the textarea when Textile is the current input filter."
   url "http://yourwebsite.com/textile_editor"
   
